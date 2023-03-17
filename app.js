0e0c1d46d9efb3978c81373ec77fc61e10621363
Leigh Scheepers <86415012+bee46137@users.noreply.github.com>
2023-03-17 08:43:19 +0200

    fixed bodmas bug

--- app.js
+++ app.js
@@ -16,3 +16,28 @@ function backspace() {
     let exp = result.value;
     result.value = exp.substring(0, exp.length - 1);
 }
+
+function calculate() {
+    let input = document.getElementById("finalResult").value;
+    let values = input.split(/[\+\-\*\/]/);
+    let operators = input.match(/[\+\-\*\/]/g);
+  }
+  
+
+let finalResult = Number(values[0]);
+
+for (let i = 0; i < operators.length; i++) {
+    let nextValue = Number(values[i + 1]);
+    if (operators[i] === "*") {
+        finalResult *= nextValue;
+    } else if (operators[i] === "/") {
+        finalResult /= nextValue;
+    } else if (operators[i] === "+") {
+        finalResult += nextValue;
+    } else if (operators[i] === "-") {
+        finalResult -= nextValue;
+    }
+}
+
+document.getElementById("result").value = finalResult;
+
