cb52ac12a470bcd2ae59a26c82392df8d4760975
ShishKirill <119962653+ShishKirill@users.noreply.github.com>
2023-03-17 16:00:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -17,27 +17,5 @@ function backspace() {
     result.value = exp.substring(0, exp.length - 1);
 }
 
-function calculate() {
-    let input = document.getElementById("finalResult").value;
-    let values = input.split(/[\+\-\*\/]/);
-    let operators = input.match(/[\+\-\*\/]/g);
-  }
-  
 
-let finalResult = Number(values[0]);
-
-for (let i = 0; i < operators.length; i++) {
-    let nextValue = Number(values[i + 1]);
-    if (operators[i] === "*") {
-        finalResult *= nextValue;
-    } else if (operators[i] === "/") {
-        finalResult /= nextValue;
-    } else if (operators[i] === "+") {
-        finalResult += nextValue;
-    } else if (operators[i] === "-") {
-        finalResult -= nextValue;
-    }
-}
-
-document.getElementById("result").value = finalResult;
 
