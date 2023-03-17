7f375cbaa63f37da67755b41e9496e9842db72fa
ShishKirill <119962653+ShishKirill@users.noreply.github.com>
2023-03-17 16:48:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const result = document.querySelector('.result');
+let hasDecimal = false;
 
 function getResult(value) {
     if (value == '=') {
         result.value = eval(result.value);
     } else {
         result.value += value;
+        hasDecimal = false;
     }
 }
 
 function clearResult() {
     result.value = '';
+    hasDecimal = false;
 }
 
 function backspace() {
@@ -17,5 +20,17 @@ function backspace() {
     result.value = exp.substring(0, exp.length - 1);
 }
 
+function addNum(num) {
+    if (result.value == '') {
+        result.value += num;
+    } else {
+        result.value += num;
+    }
+}
 
-
+function addDecimal() {
+    if (!hasDecimal) {
+        result.value += ".";
+        hasDecimal = true;
+    }
+}
