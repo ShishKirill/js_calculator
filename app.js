04367cba57cc378737af24d66254c08f91bdb565
ShishKirill <119962653+ShishKirill@users.noreply.github.com>
2023-03-20 20:23:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,76 @@
-const result = document.querySelector('.result');
+let display = document.querySelector(".result");
 let hasDecimal = false;
 
-function getResult(value) {
-    if (value == '=') {
-        result.value = eval(result.value);
-    } else {
-        result.value += value;
-        hasDecimal = false;
+function appendToDisplay(value) {
+    if (value === "*" || value === "/" || value === "+" || value === "-") {
+        display.value = display.value.replace(/\+|\-|\*|\//g, '') + value;
+    }
+    else {
+        display.value += value;
     }
 }
 
-function clearResult() {
-    result.value = '';
+function clearDisplay() {
+    display.value = "";
     hasDecimal = false;
-}
-
-function backspace() {
-    let exp = result.value;
-    result.value = exp.substring(0, exp.length - 1);
-}
-
-function addNum(num) {
-    if (result.value == '') {
-        result.value = num;
-    } else {
-        result.value += num;
-    }
 }
 
 function addDecimal() {
     if (!hasDecimal) {
-        result.value += ".";
+        display.value += ".";
         hasDecimal = true;
     }
 }
+
+function backspace() {
+    let exp = display.value;
+    display.value = exp.substring(0, exp.length - 1);
+    hasDecimal = false;
+}
+
+function calculate() {
+    let calculation = display.value.replace(/\s/g, '');
+    let pattern = /(\d+\.?\d*)([\+\-\*\/])(\d+\.?\d*)/;
+    let match = pattern.exec(calculation);
+
+    while (match) {
+        let leftOperand = parseFloat(match[1]);
+        let operator = match[2];
+        let rightOperand = parseFloat(match[3]);
+        let result = 0;
+        switch (operator) {
+            case "+":
+                result = leftOperand + rightOperand;
+                break;
+            case "-":
+                result = leftOperand - rightOperand;
+                break;
+            case "*":
+                result = leftOperand * rightOperand;
+                break;
+            case "/":
+                result = leftOperand / rightOperand;
+                break;
+        }
+        calculation = calculation.replace(match[0], result);
+        match = pattern.exec(calculation);
+    }
+    display.value = parseFloat(calculation).toFixed(6);
+}
+
+document.addEventListener("keydown", (event) => {
+    const key = event.key;
+
+    if (/\d|\.|\+|\-|\*|\//.test(key)) {
+        event.preventDefault();
+        appendToDisplay(key);
+    }
+    if (key === "Enter") {
+        event.preventDefault();
+        calculate();
+    }
+    if (key === "Backspace") {
+        event.preventDefault();
+        display.value = display.value.slice(0, -1);
+    }
+});
